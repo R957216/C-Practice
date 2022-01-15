accb2ab490aafb511a91a275f8991d62d84724eb
Rahul Kumar <ry791860@gmail.com>
2022-01-15 22:40:57 +0530

    First Commit

---
+++ Sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the 1st Number "<<endl;
+    cin>>a;
+    cout<<"Enter the the 2nd number"<<endl;
+    cin>>b;
+    cout<<"sum is :"<<a+b<<endl;
+
+
+}
